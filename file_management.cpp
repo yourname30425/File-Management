2a8c7b327e0260b5e75026c9d01085886eddeab7
yourname30425 <manh.nd233519@sis.hust.edu.vn>
2024-11-28 20:47:23 +0700

    add file_management.cpp and file_ management.h in the project

--- file_management.cpp
+++ file_management.cpp
@@ -1,32 +1,34 @@
-#include<iostream>
-#include<string>
-#include<vetor>
+#include"file_management.h"
 
 using namespace std;
 
-struct TreeNode{
-    string name;
-    bool isFolder;
-    TreeNode* parent;
-    vector<TreeNode*> childent;
-    TreeNode(string& n,TreeNode* p):name(n),isFolder(n.find(".")==std::string::npos),parent(p){}
-}
-void addChild(TreeNode* parent,const string& name){
-    TreeNode* child = new TreeNode(name,parent);
-    parent->childrent.pushback(child);
+TreeNode::TreeNode(const std::string& n, TreeNode* p,bool isF) : name(n), isFolder(isF), parent(p) {}
+
+
+void addChild(TreeNode* parent, const std::string& n, bool isF){ {
+    TreeNode* child = new TreeNode(n, parent,isF);
+    parent->children.push_back(child);
 }
 TreeNode* findChild(TreeNode* parent, const string& name){
-    for(TreeNode* child : parent->childent){
+    for(TreeNode* child : parent->children){
         if(child->name == name) return child;
     }
     return nullptr;
 }
 void listDirectory(TreeNode*node){
-    for(TreeNode*TChild : node->child) cout<< (TChild->isFolder? " [Folder] " :" [File] ")<< TChild->name<<"\n";
+    for(TreeNode*TChild : node->children) cout<< (TChild->isFolder? " [Folder] " :" [File] ")<< TChild->name<<"\n";
+}
+void deleteNode(TreeNode*node){
+    if(node->children!=nullptr){
+        for(TreeNode*child : node->children){
+            deleteNode(child);
+        }
+    }
+    delete node;
 }
 void printPath(TreeNode*node){
     if(node->parent==nullptr) return;
     printPath(node->parent);
-    cout<< node->name<<"\";
+    cout<< node->name<<'/';
 }
 
---
+++ file_management.h
@@ -0,0 +1,19 @@
+#ifndef FILE_MANAGEMENT_H
+#define FILE_MANAGEMENT_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct TreeNode {
+    std::string name;
+    bool isFolder;
+    TreeNode* parent;
+    std::vector<TreeNode*> children;
+    TreeNode(const std::string& n, TreeNode* p, bool isF); // Updated constructor
+};
+void addChild(TreeNode* parent, const std::string& name,bool isF);
+TreeNode* findChild(TreeNode* parent, const std::string& name);
+void listDirectory(TreeNode*node);
+void printPath(TreeNode*node);
+#endif // FILE_MANAGEMENT_H
