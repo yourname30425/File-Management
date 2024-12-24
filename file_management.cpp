e11f25d3643b023710fc98073817249aa3338b0b
Tduy9 <truongduy2005910@gmail.com>
2024-12-25 00:28:28 +0700

    Add files via upload

--- file_management.cpp
+++ file_management.cpp
@@ -23,6 +23,17 @@ void listDirectory(TreeNode*node){
     for(TreeNode*TChild : node->children) cout<< (TChild->isFolder? " [Folder] " :" [File] ")<< TChild->name<<"\n";
 }
 
+// Hàm đệ quy để liệt kê tất cả thư mục và file từ thư mục hiện tại
+void listSubtree(TreeNode* node, const string& indent) {
+    // Hiển thị node hiện tại
+    cout << indent << (node->isFolder ? "[Folder] " : "[File] ") << node->name << "\n";
+    
+    // Duyệt qua tất cả các thư mục và file con
+    for (TreeNode* child : node->children) {
+        listSubtree(child, indent + "  ");  // Thêm indent cho node con
+    }
+}
+
 // Xóa thư mục con/file
 void deleteNode(TreeNode*node){
     if(!node->children.empty()){
@@ -121,24 +132,13 @@ void showHelp() {
     cout << "8. touch <file>        - Create a new file with name <file>.\n";
     cout << "9. rm <file>           - Remove a file with name <file>.\n";
     cout << "10. rmdir <dir>        - Remove a directory with name <dir>.\n";
-    cout << "11. find <name>        - Find and print the path of a folder with name <name>.\n";
+    cout << "11. find <name>        - Find and print the path of a folder/a file with name <name>.\n";
     cout << "12. sort               - Sort the contents of the current directory by name.\n";       
     cout << "13. rename <old> <new> - Rename a file from <old> name to <new> name.\n";
     cout << "14. help               - Show all commands and their usage\n";
     cout << "-------------------------------------------\n";
     cout << "Type any of these commands to perform the action.\n";
     cout<<"\033[0m";
-}
-
-// Hàm đệ quy để liệt kê tất cả thư mục và file từ thư mục hiện tại
-void listSubtree(TreeNode* node, const string& indent) {
-    // Hiển thị node hiện tại
-    cout << indent << (node->isFolder ? "[Folder] " : "[File] ") << node->name << "\n";
-    
-    // Duyệt qua tất cả các thư mục và file con
-    for (TreeNode* child : node->children) {
-        listSubtree(child, indent + "  ");  // Thêm indent cho node con
-    }
 }
 
 // Hàm để chuyển đến thư mục gốc (root)
